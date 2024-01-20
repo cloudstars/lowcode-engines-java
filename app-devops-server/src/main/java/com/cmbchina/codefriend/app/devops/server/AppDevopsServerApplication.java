46c968ea1c7806f9f4746f3c8837f00ee669a0c5
cloudstars <cloudstars@zju.edu.cn>
2024-01-20 10:49:44 +0800

    添加了server工程

---
+++ app-devops-server/src/main/java/com/cmbchina/codefriend/app/devops/server/AppDevopsServerApplication.java
@@ -0,0 +1,15 @@
+package com.cmbchina.codefriend.app.devops.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用开发&运维Server启动类
+ */
+@SpringBootApplication
+public class AppDevopsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDevopsServerApplication.class, args);
+    }
+}
