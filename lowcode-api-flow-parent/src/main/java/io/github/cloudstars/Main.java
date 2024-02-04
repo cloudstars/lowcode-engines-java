3d9b06cc25831797efdeb298c3db9c2a7c14b692
RonnieLyu <wxlyu@outlook.com>
2024-02-04 10:11:30 +0800

    增加api-flow模块

---
+++ lowcode-api-flow-parent/src/main/java/io/github/cloudstars/Main.java
@@ -0,0 +1,12 @@
+package io.github.cloudstars;
+
+/**
+ * @author 80345746
+ * @version v1.0
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
