9923194bb745b154478fb0a0955c0a9eff68abd1
wuyuefeng <565948592@qq.com>
2024-02-06 15:51:06 +0800

    补充apiflow执行器

---
+++ lowcode-api-flow-parent/api-flow-engine/src/main/java/net/cf/api/flow/engine/ApiFlowExecutor.java
@@ -0,0 +1,13 @@
+package net.cf.api.flow.engine;
+
+
+import net.cf.api.flow.engine.entity.ExecuteResult;
+
+/**
+ * @Description: 编排执行器
+ * @Author: Wu Yuefeng
+ * @Date: Created on 2024/2/5
+ */
+public interface ApiFlowExecutor {
+    ExecuteResult execute();
+}
