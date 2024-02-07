99d240b767d1b3523e731caf609bfe2b9e0e9873
syd520zy <529477025@qq.com>
2024-02-07 10:51:26 +0800

    operateBean增加get/set方法

--- lowcode-commons-parent/commons-lang/src/main/java/net/cf/commons/lang/OperateBean.java
+++ lowcode-commons-parent/commons-lang/src/main/java/net/cf/commons/lang/OperateBean.java
@@ -18,4 +18,20 @@ public class OperateBean {
      * 操作时间
      */
     private Date time;
+
+    public Operator getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Operator operator) {
+        this.operator = operator;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
 }
