3637bced149d510a3938901c5fc783f97061495d
hulingyun <1335707976@qq.com>
2024-02-21 16:09:13 +0800

    ExcelEngine接口修改

--- lowcode-excel-parent/excel-engine/src/main/java/net/cf/excel/engine/ExcelEngine.java
+++ lowcode-excel-parent/excel-engine/src/main/java/net/cf/excel/engine/ExcelEngine.java
@@ -3,6 +3,9 @@ package net.cf.excel.engine;
 import net.cf.excel.engine.config.ExcelImportConfig;
 import org.apache.poi.ss.usermodel.Workbook;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Excel服务
  *
@@ -11,17 +14,18 @@ import org.apache.poi.ss.usermodel.Workbook;
 public interface ExcelEngine {
 
     /**
-     * 导入Excel
-     *
      * @param config
      * @param workbook
+     * @return List<Map<String,Object>>
+     * @description 解析一个Excel文件,返回解析出的数据
      */
-    void doImport(ExcelImportConfig config, Workbook workbook);
+    List<Map<String, Object>> parseExcel(Workbook workbook, ExcelImportConfig config);
 
     /**
-     * 导出Excel
-     *
+     * @param data
      * @param config
+     * @return Workbook
+     * @description 基于数据和配置生成Excel
      */
-    Workbook export(ExcelImportConfig config);
+    Workbook buildExcel(List<Map<String, Object>> data, ExcelImportConfig config);
 }
